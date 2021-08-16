c3c73577f11e0a6b0082a4588eef78f69e08cbf2
KevChaplin <kevchaplin44@gmail.com>
2021-08-16 11:05:07 +0900

    gh-pages.js and others set - deploying live page

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', public
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/KevChaplin/rate-my-racing',
+		user: {
+			name: 'Kevin Chaplin', // update to use your name
+			email: 'kevchaplin44@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
